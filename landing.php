f47c99426111ca8ab272a049eb0cc8d651ef11f0
mhuss044 <mhuss044@gmail.com>
2014-04-06 18:24:55 -0400

    Update landing.php

--- landing.php
+++ landing.php
@@ -1,11 +1,13 @@
 <?php
 
-  feedURL = 'https://gdata.youtube.com/feeds/api/videos/'.$line.'?v=2';
+	// $line here has to be the video ID
+	$feedURL = 'https://gdata.youtube.com/feeds/api/videos/'.$line.'?v=2';
 	// read feed into SimpleXML object
 	$sxml = simplexml_load_file($feedURL);
-
-	echo "<br>				The youtube video of ";
 	
+	echo "<br> The youtube video at this rss has title: ".$sxml->title;
 	
+	// Apparently thisll dump teh whole structure, branches, etc
+	echo $sxml;
 	
 ?>
